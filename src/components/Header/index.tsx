c0c4a0e90c9693b85161c8c8bf9c4d47cd5114ba
Tejas <tejas.kntk@gmail.com>
2022-03-23 17:02:08 +0800

    navigate to home on clicking header title;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header: React.FC = () => {
   return (
-    <header className="items-center px-8 py-6 border-b border-solid border-primary-light">
-      <h3 className="text-xl text-center font-bold uppercase">Contact Management</h3>
+    <header className="px-8 py-6 border-b border-solid border-primary-light">
+      <Link to="/" className="text-xl text-center font-bold uppercase">Contact Management</Link>
     </header>
   )
 }
