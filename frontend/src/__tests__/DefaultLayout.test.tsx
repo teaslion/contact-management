13a0032193f6a13410138e60143b7b1fd756afab
Tejas <tejas.kntk@gmail.com>
2022-03-25 00:33:37 +0800

    unit test for Header, DefaultLayout;

---
+++ frontend/src/__tests__/DefaultLayout.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { DefaultLayout } from 'layouts/DefaultLayout';
+
+describe('DefaultLayout', () => {
+  test('should have header title', () => {
+    render(<DefaultLayout />, { wrapper: MemoryRouter });
+    const titleElement = screen.getByText(/contact management/i);
+    expect(titleElement).toBeInTheDocument();
+  })
+
+  test('should render the child component', () => {
+    render(<DefaultLayout>
+      <h3>Demo Content</h3>
+    </DefaultLayout>, { wrapper: MemoryRouter });
+
+    const childElement = screen.getByText(/demo content/i);
+    expect(childElement).toBeInTheDocument();
+  })
+})
---
+++ frontend/src/__tests__/Header.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Header } from 'components/Header';
+
+describe('Header Component', () => {
+  test('Should render header title', () => {
+    render(<Header />, { wrapper: MemoryRouter })
+    const linkElement = screen.getByText(/contact management/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute('href', '/')
+  })
+
+  test('Should have anchor to /', () => {
+    render(<Header />, { wrapper: MemoryRouter })
+    const linkElement = screen.getByText(/contact management/i);
+    expect(linkElement).toHaveAttribute('href', '/')
+  })
+
+  test('Should have the valid classes.', () => {
+    render(<Header />, { wrapper: MemoryRouter })
+    const linkElement = screen.getByText(/contact management/i);
+    expect(linkElement).toHaveClass('text-xl text-center font-bold uppercase');
+  })
+})
